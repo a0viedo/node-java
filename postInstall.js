f667d2ba0ab83de406cca52ffbbb8c77347d4528
silvio <silvio.massari@gmail.com>
2014-10-24 13:59:24 -0300

    should inform error when java is not installed

--- postInstall.js
+++ postInstall.js
@@ -4,7 +4,13 @@ var glob = require('glob');
 var path = require('path');
 var pathDelimeterNode0_8 = (os.platform()==='win32')  ? ";" : ":";
 
-require('find-java-home')( function(err, home){
+require('find-java-home')( function (err, home) {
+    if (err) {
+        console.log("Couldn't find JAVA");
+        console.log(err);
+        process.exit(1)
+    }
+    
     var binary;
 
     if(home) {
@@ -13,6 +19,11 @@ require('find-java-home')( function(err, home){
             glob.sync('**/libjvm.so', {cwd: home})[0] ||
             glob.sync('**/libjvm.dylib', {cwd: home})[0];
 
+        if (!binary) {
+            console.log("\n\nJAVA runtime is missing.");
+            process.exit(1);
+        }
+        
         fs.writeFileSync(
             path.resolve(__dirname, './build/jvm_dll_path.json'),
             binary
