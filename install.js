3bcb6cb33e352bfa7611179eab6f96bec8f50b08
silvio <silvio.massari@gmail.com>
2014-10-24 13:51:48 -0300

    jvm.dll was removed from prebuild folder

--- install.js
+++ install.js
@@ -14,14 +14,17 @@ if (process.version.indexOf("v0.10") === 0) {
     return process.exit(1); 
 }
 
+var sourceCommonDir = path.join(__dirname, "prebuild", os.platform(), os.arch(), "common");
 var sourceDir = path.join(__dirname, "prebuild", os.platform(), os.arch(), nodeVersion);
 var source = path.join(sourceDir, bindingFile);
 
 var targetDir = path.join(__dirname, "build", "Release");
 
 console.log("install.js settings:");
+console.log("\t", sourceCommonDir);
 console.log("\t", sourceDir);
 console.log("\t", targetDir);
+console.log();
 
 if (!fs.existsSync(source)) {
 
@@ -48,38 +51,41 @@ var cp = function(src, dest, cb) {
     var count = 0;
 
     var copyFile = function(src, dest, cb) {
-        var writer = fs.createWriteStream(dest);
-        writer.on("end", function () {
-            console.log(src + " was copied to " + dest);
-            cb();
-        });
-        writer.on("error", function(err) {
-            console.log("Can not copy " + src + " to " + dest);
-            cb(err);
-        });
-        fs.createReadStream(src).pipe(writer);   
+        try {
+            var reader = fs.createReadStream(src);
+
+            reader.on("end", function () {
+                console.log("\t", src, " > ", dest, "> SUCCEED!");
+                cb();
+            });
+            reader.once("error", function (err) {
+                console.log("\t", src, " > ", dest, "> FAILED", err);
+                cb(err);
+            });
+
+            reader.pipe(fs.createWriteStream(dest));   
+        } catch (e) {
+            console.log("\t", src, " > ", dest, "> FAILED", e);
+        }
     };
 
     var copyRecursive = function(src, dest, cb) {
-        console.log ("copy", src, dest);
         var exists = fs.existsSync(src);
         var stats = exists && fs.statSync(src);
         var isDirectory = exists && stats.isDirectory();
 
         if (isDirectory) {
             if (!fs.existsSync(dest)) fs.mkdirSync(dest);
-            fs.readdirSync(src).forEach(function(childItemName) {
-                count++;
+            fs.readdirSync(src).forEach(function (childItemName) {
                 copyRecursive(path.join(src, childItemName),
-                    path.join(dest, childItemName), function(err) {
-                        cont--;
+                    path.join (dest, childItemName), function (err) {
+                        if (count===0) cb(err);
                     }
                 );
             });
-            if (count===0) cb();
         } else {
             count++;
-            copyFile(src, dest, function(err) {
+            copyFile(src, dest, function (err) {
                 count--;
                 if (count===0 || err) cb(err);
             });
@@ -90,8 +96,24 @@ var cp = function(src, dest, cb) {
 };
 
 mkdir(targetDir);
-cp(sourceDir, targetDir, function (err){
+
+console.log("Copying files from:", sourceCommonDir);
+
+cp(sourceCommonDir, targetDir, function (err) {
     "use strict";
-    process.exit(err ? 1 : 0);
+    if (err) { 
+        console.log("Error:", err);
+        process.exit(1);
+    }
+
+    console.log("\nCopying files from:", sourceDir);
+    cp(sourceDir, targetDir, function (err){
+        "use strict";
+        if (err) { 
+            console.log("Error:", err);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
 });
 
--- postInstall.js
+++ postInstall.js
@@ -4,37 +4,23 @@ var glob = require('glob');
 var path = require('path');
 var pathDelimeterNode0_8 = (os.platform()==='win32')  ? ";" : ":";
 
-if (os.platform()==='win32') {
+require('find-java-home')( function(err, home){
+    var binary;
 
-    fs.writeFileSync(
-        path.resolve(__dirname, './build/jvm_dll_path.json'),
-        JSON.stringify(
-            (path.delimiter || pathDelimeterNode0_8) +
-            path.join(__dirname, "build", "Release")
-        )
-    );
+    if(home) {
+        binary = 
+            glob.sync('**/jvm.dll', {cwd: home})[0] ||
+            glob.sync('**/libjvm.so', {cwd: home})[0] ||
+            glob.sync('**/libjvm.dylib', {cwd: home})[0];
 
-} else {
-
-    require('find-java-home')( function(err, home){
-        var dylib;
-        var so;
-        var binary;
-
-        if(home) {
-            so = glob.sync('**/libjvm.so', {cwd: home})[0];
-            dylib = glob.sync('**/libjvm.dylib', {cwd: home})[0];
-            binary = dylib || so;
-
-            fs.writeFileSync(
-                path.resolve(__dirname, './build/jvm_dll_path.json'),
-                binary
-                ? JSON.stringify(
-                    (path.delimiter || pathDelimeterNode0_8)
-                    + path.dirname(path.resolve(home, binary))
-                    )
-                : '""'
-            );
-        }
-    });
-}
+        fs.writeFileSync(
+            path.resolve(__dirname, './build/jvm_dll_path.json'),
+            binary
+            ? JSON.stringify(
+                (path.delimiter || pathDelimeterNode0_8)
+                + path.dirname(path.resolve(home, binary))
+                )
+            : '""'
+        );
+    }
+});
